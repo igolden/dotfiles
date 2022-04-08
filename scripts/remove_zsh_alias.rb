6942a683bcc8e0680908901a03a20486c38eda9c
Ian Golden <ian@iangolden.com>
2022-04-07 21:02:56 -0500

    Added getter/setter for zsh aliases

--- scripts/remove_zsh_alias.rb
+++ scripts/remove_zsh_alias.rb
@@ -3,51 +3,29 @@ require 'pry'
 require 'fileutils'
 require 'tempfile'
 
-def remove_lines(input_file)
+def remove_lines(input_file, matcher)
   File.open("#{input_file}.new", "w+") do |tmp|
     File.foreach(input_file) do |line|
-      tmp << line unless line.include?("b")
+      tmp << line unless line.include?("alias #{matcher}=")
     end
   end
 
   FileUtils.mv("#{input_file}.new", input_file)
 end
 
-remove_lines("#{Dir.home}/Desktop/test_file.txt")
 
 
-#new_alias = ARGV[0]
-
-#if ARGV.first.nil?
-  #puts "Supply an alias or gtfo"
-  #exit(1)
-#end
+if ARGV.first.nil?
+  puts "Supply an alias or gtfo"
+  exit(1)
+end
 
 
-#if ARGV.first.match?(/\w*=\w*/)
-  #_alias, _cmd = ARGV.first.split("=")
-
-  ## read existing aliases and find matches
-  #d = File.read("#{Dir.home}/.zsh_aliases")
-  #m = d.scan(/^alias.*$/)
-
-  #all_cmds = m.map do |cmd|
-    #cmd.match(/(?<=alias\s).*$/).to_s.split("=").first
-  #end
-
-  #if all_cmds.include?(_cmd)
-    #puts "Alias already exists, modify it instead."
-    #puts m.filter {|x| x.test(/^alias\s#{_cmd}.*$/)}.first
-  #end
-
-  #puts "Adding alias.."
-
-  #File.open("#{Dir.home}/.zsh_aliases", "a+") do |f|
-    #f << "alias #{ARGV.first}"
-  #end
-
-  #puts "Added #{_alias}"
-#else
-  #puts "Alias syntax is bad, try again."
-  #exit(1)
-#end
+if ARGV.first.to_s.include?(" ")
+  puts "Alias should be a single word no spaces"
+  exit(1)
+else
+  puts "Removing #{ARGV.first} alias"
+  remove_lines("#{Dir.home}/.zsh_aliases", ARGV.first)
+  puts "Done."
+end
