261f4e28d06edeab515a942541f52168a5ab8a67
Ian Golden <ian@iangolden.com>
2022-04-09 21:43:51 -0500

    Organizing and dialing in for workspace

--- bin/install.sh
+++ bin/install.sh
@@ -2,29 +2,29 @@
 
 function install_vim_config {
 	echo "Installing vim config.."
-	ln -s ~/dotfiles/vimrc ~/.vimrc
-  rm -rf ~/.vim
-  ln -s ~/dotfiles/vim ~/.vim
+	ln -s $HOME/dotfiles/config/vimrc $HOME/.vimrc
+  rm -rf $HOME/.vim
+  ln -s $HOME/dotfiles/config/vim $HOME/.vim
 }
 
 function install_git {
-  ln -s ~/dotfiles/gitconfig ~/.gitconfig
-  ln -s ~/dotfiles/githelpers ~/.githelpers
+  ln -s $HOME/dotfiles/config/gitconfig $HOME/.gitconfig
+  ln -s $HOME/dotfiles/config/githelpers $HOME/.githelpers
 }
 
 function install_zsh {
-  ln -s ~/dotfiles/zshrc ~/.zshrc
-  ln -s ~/dotfiles/zprofile ~/.zprofile
-  ln -s ~/dotfiles/zsh_aliases ~/.zsh_aliases
-  ln -s ~/dotfiles/zsh_functions ~/.zsh_functions
+  ln -s $HOME/dotfiles/config/zshrc $HOME/.zshrc
+  ln -s $HOME/dotfiles/config/zprofile $HOME/.zprofile
+  ln -s $HOME/dotfiles/config/zsh_aliases $HOME/.zsh_aliases
+  ln -s $HOME/dotfiles/config/zsh_functions $HOME/.zsh_functions
 }
 
 function install_antigen {
 	curl -L git.io/antigen > $HOME/antigen.zsh
 }
 function install_vim_plugins {
-  mkdir -p ~/dotfiles/vim/bundle
-  cd ~/dotfiles/vim/bundle
+  mkdir -p $HOME/dotfiles/config/vim/bundle
+  cd $HOME/dotfiles/config/vim/bundle
   git clone https://github.com/jiangmiao/auto-pairs 
   git clone https://github.com/jlanzarotta/bufexplorer.git 
   git clone https://github.com/itchyny/calendar.vim 
@@ -38,26 +38,26 @@ function install_vim_plugins {
   git clone https://github.com/guns/xterm-color-table.vim 
   git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git 
   git clone https://github.com/prettier/vim-prettier.git 
-  cd ~
+  cd $HOME
 }
 function install_command_t {
-  #cd ~/dotfiles/vim/bundle
+  #cd $HOME/dotfiles/config/vim/bundle
   #git clone https://github.com/wincent/command-t
   cd command-t/ruby/command-t/ext/command-t
   ruby extconf.rb
   make
-  cd ~
+  cd $HOME
 }
 function install_vim_syntax {
 	echo "Installing vim syntax.."
-  cd ~/dotfiles/vim/bundle
+  cd $HOME/dotfiles/config/vim/bundle
   git clone https://github.com/fatih/vim-go 
   git clone https://github.com/rhysd/vim-crystal
   git clone https://github.com/rust-lang/rust.vim
   git clone https://github.com/keith/swift.vim
   git clone https://github.com/pangloss/vim-javascript
   git clone https://github.com/mxw/vim-jsx
-  cd ~
+  cd $HOME
 }
 
 function install_powerline_fonts {
@@ -68,7 +68,7 @@ function install_powerline_fonts {
 }
 
 
-install_antigen
+#install_antigen
 install_git
 install_zsh
 install_vim_config
